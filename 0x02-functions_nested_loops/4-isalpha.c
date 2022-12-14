d9b0e2916eb5833fb90fea1836b1c54e3a7d90dd
Derikoo <derickngururi@gmail.com>
2022-12-14 04:28:15 -0800

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: Returns 1 if c is a letter, lowercase or uppercase or 0 something else
+ * Return: 0
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+	return (1);
+	}
+	return (0);
+}
