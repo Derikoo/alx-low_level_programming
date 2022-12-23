f1dba2de1246f9e7c1c4b25d9680223d1e09a58d
Derikoo <derickngururi@gmail.com>
2022-12-22 22:44:27 -0800

    sun

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ * Return: 10 times the number from 0 to 14
+ */
+
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+	for (y = 0; y <= 14; y++)
+	{
+	if (y > 9)
+	{
+	_putchar(x + '0');
+	}
+	_putchar(y + '0');
+	}
+	_putchar('\n');
+	}
+}
