cbef0f43bcc6626d4227190efd7fce77b618923d
Derikoo <derickngururi@gmail.com>
2022-12-24 22:02:45 -0800

    plug

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,11 +14,11 @@ int main(void)
 	{
 	if ((x % 3 == 0) && (x % 5 == 0))
 	{
-	printf("fizzbuzz");
+	printf("FizzBuzz");
 	}
 	else if (x % 3 == 0)
 	{
-	printf("fizz");
+	printf("Fizz");
 	}
 	else if (x % 5 == 0)
 	{
