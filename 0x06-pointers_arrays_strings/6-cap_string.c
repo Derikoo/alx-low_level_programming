0ed35df75b8db3ad883f593c5e4cc91f324d3f75
Derick <derickngururi@gmail.com>
2023-01-15 07:23:50 -0800

    fit

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,11 +9,12 @@
 char *cap_string(char *str)
 {
 	int index = 0;
+
 	while (str[index])
 	{
 	while (!(str[index] >= 'a' && str[index] <= 'z'))
 	index++;
-	if (str[index - 1] == ' '||
+	if (str[index - 1] == ' ' ||
 	str[index - 1] == '\t' ||
 	str[index - 1] == '\n' ||
 	str[index - 1] == ',' ||
